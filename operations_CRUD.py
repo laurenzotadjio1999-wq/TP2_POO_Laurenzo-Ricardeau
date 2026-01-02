8a59d6996774687caa4120998d5d3d7555ac8c3f
durandrica-hue <durandrica@gmail.com>
2026-01-02 18:08:19 -0500

    fonction inserer dans Annuaire

---
+++ operations_CRUD.py
@@ -0,0 +1,12 @@
+import sqlite3
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QTableWidget, QTableWidgetItem
+
+def InsererDansAnnuaire():
+    print("inser dans la table")
+    conn = sqlite3.connect("Annuaire.db")
+    cursor = conn.cursor()
+    # requette ici
+    cursor.execute(
+        "INSERT INTO Annuaire VALUES (" + lineEditNom.text() + ",'" + lineEditPreNom.text() + "','" + lineEditcourriel.text() + "','" + lineEditTelephone.text() + "');")
+    conn.commit()
+    AfficherTout()
